c2e95d3562d4633109494a65fceea199bc0fc5e5
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-12-25 17:45:31 -0500

    Updated sln.
    Updated sln.

---
+++ intro.time.series/05 - moving.average/02 - ar_vs_ma.R
@@ -0,0 +1,15 @@
+# Find correlation between AR_fit and MA_fit
+cor(AR_fit, MA_fit)
+
+# Find AIC of AR
+AIC(AR)
+
+# Find AIC of MA
+AIC(MA)
+
+# Find BIC of AR
+BIC(AR)
+
+# Find BIC of MA
+BIC(MA)
+
