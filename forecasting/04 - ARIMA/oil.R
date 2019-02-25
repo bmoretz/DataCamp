96df2dd67381f74fd3a2440e587c15466ebcca39
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-02-25 16:56:18 -0500

    Forecasting, ch4
    Forecasting, ch4

---
+++ forecasting/04 - ARIMA/oil.R
@@ -0,0 +1,8 @@
+# Create an autoplot of the oil data
+autoplot(oil)
+
+# Create a lag plot of the oil data
+gglagplot(oil)
+
+# Create an ACF plot of the oil data
+ggAcf(oil)
---
+++ forecasting/04 - ARIMA/sunspots.R
@@ -0,0 +1,13 @@
+# Plot the annual sunspot numbers
+autoplot(sunspot.year)
+ggAcf(sunspot.year)
+
+# Save the lag corresponding to maximum autocorrelation
+maxlag_sunspot <- 1
+
+# Plot the traffic on the Hyndsight blog
+autoplot(hyndsight)
+ggAcf(hyndsight)
+
+# Save the lag corresponding to maximum autocorrelation
+maxlag_hyndsight <- 7
